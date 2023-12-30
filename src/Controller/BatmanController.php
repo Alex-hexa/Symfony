c0ee6f61d4d1a58759ed936e64d44234bcf18243
Alex-Hexa <alexandre.mulard@ecole-hexagone.com>
2023-12-30 12:45:17 +0100

    batman

---
+++ src/Controller/BatmanController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BatmanController extends AbstractController
+{
+    #[Route("/batman", name:"batman")]
+    public function FunctionName(): Response{
+        return $this->render('batman.html.twig');
+    }
+}
